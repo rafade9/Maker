b52747212ff80233c774a05edc1fe4b2ea3388ec
rafade9 <rafade9@gmail.com>
2015-10-02 13:55:33 -0500

    Ajustes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,9 @@
     makerApp.controller('MakerController', function($scope, $sce){
         $scope.jsonHtml = {id:1, html: $sce.trustAsHtml("<strong>Prueba<\/strong>")};
 
-        $scope.fields = [{id:1, html:$sce.trustAsHtml("<input type:\"text\" ng-model=\"nombre\">")},
-                         {id:1, html:$sce.trustAsHtml("<input type:\"text\" ng-model=\"paterno\">")},
-                         {id:1, html:$sce.trustAsHtml("<input type:\"text\" ng-model=\"materno\">")}];
+        $scope.fields = [{id:1, tag:'Nombre', html:$sce.trustAsHtml("<input type=\"text\" class=\"form-control\" ng-model=\"nombre\">")},
+                         {id:1, tag:'Apellido Paterno', html:$sce.trustAsHtml("<input class=\"form-control\" type=\"text\" ng-model=\"paterno\">")},
+                         {id:1, tag:'Apellido Materno', html:$sce.trustAsHtml("<input class=\"form-control\" type=\"text\" ng-model=\"materno\">")},
+                         {id:1, tag:'Edad', html:$sce.trustAsHtml("<input class=\"form-control\" type=\"number\" ng-model=\"edad\">")}];
     });
 })();
